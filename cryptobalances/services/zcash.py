fe812d58c529c7b6f9fa8b19d8570df38338fdbe
Aliaksandr Leonau <leonov.aleksandr.1987@gmail.com>
2017-02-16 08:23:15 -0500

    Api for zcash currency started returning 403 error with default user-agent. Implemented hot fix but we need understand why it's happening.

--- cryptobalances/services/zcash.py
+++ cryptobalances/services/zcash.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 from urllib.request import urlopen
+from urllib.request import Request
 from urllib.error import URLError, HTTPError
 from cryptobalances.config import get_api_url
 
@@ -9,7 +10,15 @@ from cryptobalances.config import get_api_url
 
 def pull_request(currency, identifier):
     try:
-        with urlopen(get_api_url(currency).format(identifier=identifier), timeout=60) as f:
+        request = Request(get_api_url(currency).format(identifier=identifier), method='GET')
+
+        # If perform the request with user-agent the default for python api returns 403 http error
+        request.add_header('User-Agent',
+                           'Mozilla/5.0 (Windows NT 6.1; WOW64) '
+                           'AppleWebKit/537.36 (KHTML, like Gecko) '
+                           'Chrome/55.0.2883.87 Safari/537.36')
+
+        with urlopen(request, timeout=60) as f:
             return str(json.loads(f.read().decode('utf-8'))['balance'])
     except HTTPError as error:
         response = json.loads(error.read().decode('utf-8'))
