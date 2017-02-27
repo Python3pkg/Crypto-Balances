85c60df1b6670cedecd6eb2ac31ce635f5678b91
Aliaksandr Leonau <leonov.aleksandr.1987@gmail.com>
2017-02-27 07:15:04 -0500

    The first implementation of obtaining rates for currency pair.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join, dirname
 
 setup(
     name='cryptobalances',
-    version='0.9.2',
+    version='0.9.3',
     author='Aliaksandr Leonau',
     author_email='leonov.aleksandr.1987@gmail.com',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
